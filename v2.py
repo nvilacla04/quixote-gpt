fbe8a6e792644cf7e047d1ae4d6845a3eb74c86a
nvilacla04 <nicolaurve@gmail.com>
2026-03-05 23:57:39 +0100

    preparing next model

---
+++ v2.py
@@ -0,0 +1,21 @@
+import torch 
+import torch.nn as nn 
+from torch.nn import functional as F 
+
+
+#hyperparams
+batch_size = 32         #from 16 shorter sequences with BPE so we can fit more 
+block_size = 64         #from 32 subword carries more meaning than char
+max_iters = 5000        
+eval_interval = 100
+learning_rate = 3e-4 
+device = "cuda" if torch.cuda.is_available() else "cpu"  #will use vu servers so GPU for me 
+eval_iters = 200
+n_embd = 128 
+n_head = 4
+n_layer = 4 
+dropout = 0.1 
+
+torch.manual_seed(3456789)
+
+
